96460863124f3a81b8835171dead5d397843eac7
strdh <strdh33@gmail.com>
2022-08-03 02:45:32 +0700

    Completing pratice context

--- pratice.go
+++ pratice.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	// "time"
+	"time"
 	"context"
 	"runtime"
 )
@@ -10,7 +10,10 @@ import (
 func main() {
 	sum := make(chan int)
 	parent := context.Background()
-	ctx, cancel := context.WithCancel(parent)
+	// ctx, cancel := context.WithCancel(parent)
+	// ctx, cancel := context.WithTimeout(parent, 6*time.Second)
+	ctx, cancel := context.WithDeadline(parent, time.Now().Add(6*time.Second))
+	defer cancel()
 
 	go func() {
 		defer close(sum)
@@ -23,18 +26,25 @@ func main() {
 			default:
 				sum <- counter
 				counter++
+				time.Sleep(2 * time.Second)
 			}
 		}
 	}()
 
 	fmt.Println("Jumlah Go-Routine : ", runtime.NumGoroutine())
 
+	//with cancel
+	// for iter := range sum {
+	// 	fmt.Println("Counter : ", iter)
+	// 	if iter == 10 {
+	// 		cancel()
+	// 		// break
+	// 	}
+	// }
+
+	//with timeout
 	for iter := range sum {
 		fmt.Println("Counter : ", iter)
-		if iter == 10 {
-			cancel()
-			// break
-		}
 	}
 
 	fmt.Println("Jumlah Go-Routine : ", runtime.NumGoroutine())
